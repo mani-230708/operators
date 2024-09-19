4548e14e01b3b943822c8de1ec3326c10c3fabfd
mani-230708 <22hr1a3262@mtieat.org>
2024-09-19 14:14:21 +0530

    area.py

--- area.py
+++ area.py
@@ -31,3 +31,20 @@ y3 = int(input())
 
 area = abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
 '''
+
+def calculate_triangle_area():
+  s
+    x1 = int(input())
+    y1 = int(input())
+    x2 = int(input())
+    y2 = int(input())
+    x3 = int(input())
+    y3 = int(input())
+
+
+    area = abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
+
+    print(f"The area of the triangle is {area:.2f}")
+
+
+calculate_triangle_area()
